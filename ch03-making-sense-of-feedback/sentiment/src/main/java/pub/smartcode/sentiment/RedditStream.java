185dad8ce40a5e11f4c4b41082c50b12bf03ff56
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2018-05-14 15:23:13 -0400

    Ch03: Updated CRUX in pom.xml, more reliable Reddit client

--- ch03-making-sense-of-feedback/sentiment/src/main/java/pub/smartcode/sentiment/RedditStream.java
+++ ch03-making-sense-of-feedback/sentiment/src/main/java/pub/smartcode/sentiment/RedditStream.java
@@ -16,15 +16,25 @@ import net.dean.jraw.tree.RootCommentNode;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Properties;
+import java.net.SocketException;
 
 public class RedditStream implements Runnable {
     private RedditClient reddit;
     private SentimentDetector sentimentDetector;
     private ArrayList<String> terms;
+    private Properties props;
 
     public RedditStream(SentimentDetector sentimentDetector,
                         Properties props) {
         this.sentimentDetector = sentimentDetector;
+        terms = Lists.newArrayList(
+                props.getProperty("reddit_terms")
+                        .split("\\s*,\\s*"));
+        this.props = props;
+        setupConnection();
+    }
+
+    public void setupConnection() {
         UserAgent userAgent = new UserAgent("bot", "pub.smartcode.sentiment", "v0.1", props.getProperty("reddit_user"));
         Credentials credentials = Credentials.script(
                 props.getProperty("reddit_user"),
@@ -33,51 +43,62 @@ public class RedditStream implements Runnable {
                 props.getProperty("reddit_clientsecret"));
         NetworkAdapter adapter = new OkHttpNetworkAdapter(userAgent);
         reddit = OAuthHelper.automatic(adapter, credentials);
-
-        terms = Lists.newArrayList(
-                props.getProperty("reddit_terms")
-                        .split("\\s*,\\s*"));
     }
 
     public void run() {
         try {
             while (true) {
-                for (String term : terms) {
-                    SearchPaginator result =
+                try {
+                    for (String term : terms) {
+                        SearchPaginator result =
                             reddit.search().query(term).build();
-                    for (Submission s : result.next()) {
-                        if(!sentimentDetector.alreadyProcessed(s.getId())) {
-                            sentimentDetector.detectSentiment(
-                                    s.getId(),
-                                    s.getTitle(),
-                                    "reddit",
-                                    false, true);
-                            if (s.getSelfText() != null) {
+                        for (Submission s : result.next()) {
+                            if(!sentimentDetector.alreadyProcessed(s.getId())) {
                                 sentimentDetector.detectSentiment(
-                                        s.getId() + "text",
-                                        s.getSelfText(),
+                                        s.getId(),
+                                        s.getTitle(),
                                         "reddit",
-                                        false, true);
-                            }
-
-                            RootCommentNode comments = s.toReference(reddit)
-                                    .comments();
-                            Iterator<CommentNode<PublicContribution<?>>> it =
-                                    comments.walkTree().iterator();
-                            while (it.hasNext()) {
-                                PublicContribution<?> thing = it.next().getSubject();
-
-                                if (thing.getBody() != null &&
-                                        !sentimentDetector.alreadyProcessed(thing.getId())) {
+                                        true, true);
+                                if (s.getSelfText() != null) {
                                     sentimentDetector.detectSentiment(
-                                            thing.getId(),
-                                            thing.getBody(),
+                                            s.getId() + "text",
+                                            s.getSelfText(),
                                             "reddit",
-                                            false, true);
+                                            true, true);
+                                }
+
+                                RootCommentNode comments = s.toReference(reddit)
+                                    .comments();
+                                Iterator<CommentNode<PublicContribution<?>>> it =
+                                    comments.walkTree().iterator();
+                                while (it.hasNext()) {
+                                    PublicContribution<?> thing = it.next().getSubject();
+
+                                    if (thing.getBody() != null &&
+                                            !sentimentDetector.alreadyProcessed(thing.getId())) {
+                                        // look for at least one search term in comment
+                                        boolean containsTerm = false;
+                                        String body = thing.getBody();
+                                        for(String t : terms) {
+                                            if(body.indexOf(t) != -1) {
+                                                containsTerm = true;
+                                                break;
+                                            }
+                                        }
+                                        if(containsTerm) {
+                                            sentimentDetector.detectSentiment(
+                                                    thing.getId(),
+                                                    body,
+                                                    "reddit",
+                                                    true, true);
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+                } catch(Exception e) {
+                    setupConnection();
                 }
                 // sleep 10 minutes
                 Thread.sleep(600000);
